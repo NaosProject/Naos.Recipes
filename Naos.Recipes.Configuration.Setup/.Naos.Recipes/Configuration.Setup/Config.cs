c1b1dd0e64fa10d428efe6d62b4826ae68f4de9a
Lawson Caudill <lawson@caudillclan.us>
2017-03-10 17:58:03 -0300

    Added recipe to allow for easily configuring Its.Configuration inside unit tests in the same manner that it is done in Naos.Deployment runs.

---
+++ Naos.Recipes.Configuration.Setup/.Naos.Recipes/Configuration.Setup/Config.cs
@@ -0,0 +1,32 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Config.cs" company="CoMetrics">
+//   Copyright 2017 CoMetrics
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Naos.Recipes.Configuration.Setup
+{
+    using Its.Configuration;
+
+    using Newtonsoft.Json;
+
+    using Spritely.Recipes;
+
+    /// <summary>
+    /// Static class to hold logic to setup configuration.
+    /// </summary>
+    public static partial class Config
+    {
+        /// <summary>
+        /// Set the precedence in Its.Configuration to the supplied environment followed by "Common".
+        /// </summary>
+        /// <param name="environment">Environment name that matches the configuration folder in ".config".</param>
+        public static void SetupForUnitTest(string environment)
+        {
+            JsonConvert.DefaultSettings = () => JsonConfiguration.DefaultSerializerSettings;
+            Settings.Deserialize = (type, serialized) => JsonConvert.DeserializeObject(serialized, type);
+            Settings.SettingsDirectory = Settings.SettingsDirectory.Replace("\\bin\\Debug", string.Empty);
+            Settings.Precedence = new[] { environment, "Common" };
+        }
+    }
+}
