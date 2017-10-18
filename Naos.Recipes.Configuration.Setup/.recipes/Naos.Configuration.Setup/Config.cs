1644e4e4a07f9510d5726f78e412ae1604817acf
Lawson Caudill <lawson@caudillclan.us>
2017-10-18 19:43:26 -0300

    Added optional announcer to the Config recipe.

--- Naos.Recipes.Configuration.Setup/.recipes/Naos.Configuration.Setup/Config.cs
+++ Naos.Recipes.Configuration.Setup/.recipes/Naos.Configuration.Setup/Config.cs
@@ -9,6 +9,7 @@
 
 namespace Naos.Recipes.Configuration.Setup
 {
+    using System;
     using System.Collections.Generic;
     using System.IO;
     using System.Linq;
@@ -17,6 +18,8 @@ namespace Naos.Recipes.Configuration.Setup
 
     using Naos.Serialization.Domain;
     using Naos.Serialization.Json;
+
+    using OBeautifulCode.Collection.Recipes;
 
     using Spritely.Recipes;
 
@@ -39,9 +42,18 @@ namespace Naos.Recipes.Configuration.Setup
         /// <summary>
         /// Set up serialization logic for Newtonsoft and Its.Configuration to use when reading settings.
         /// </summary>
-        public static void ConfigureSerialization()
+        /// <param name="announcer">Optional announcer to communicate setup state; DEFAULT is null.</param>
+        public static void ConfigureSerialization(Action<string> announcer = null)
         {
+            void NullAnnouncer(string message)
+            {
+                /* no-op */
+            }
+
+            var localAnnouncer = announcer ?? NullAnnouncer;
+
             Settings.Deserialize = (type, serialized) => deserializer.Deserialize(serialized, type);
+            localAnnouncer(Invariant($"Set {nameof(Settings)}.{nameof(Settings.Deserialize)} to use {deserializer.ToString()}"));
         }
 
         /// <summary>
@@ -50,9 +62,10 @@ namespace Naos.Recipes.Configuration.Setup
         /// <param name="precedenceValue">Value to set.</param>
         /// <param name="includeCommonPrecedenceAtEnd">Optional value indicating whether or not to add the <see cref="CommonPrecedence" /> to the end of the chain; DEFAULT is true.</param>
         /// <param name="settingsDirectory">Optional settings root directory; DEFAULT is null and will look for a ".config" folder at execution location.</param>
-        public static void ResetConfigureSerializationAndSetValues(string precedenceValue, bool includeCommonPrecedenceAtEnd = true, string settingsDirectory = null)
+        /// <param name="announcer">Optional announcer to communicate setup state; DEFAULT is null.</param>
+        public static void ResetConfigureSerializationAndSetValues(string precedenceValue, bool includeCommonPrecedenceAtEnd = true, string settingsDirectory = null, Action<string> announcer = null)
         {
-            ResetConfigureSerializationAndSetValues(new[] { precedenceValue }, includeCommonPrecedenceAtEnd, settingsDirectory);
+            ResetConfigureSerializationAndSetValues(new[] { precedenceValue }, includeCommonPrecedenceAtEnd, settingsDirectory, announcer);
         }
 
         /// <summary>
@@ -61,18 +74,28 @@ namespace Naos.Recipes.Configuration.Setup
         /// <param name="precedenceValues">Values to set.</param>
         /// <param name="includeCommonPrecedenceAtEnd">Optional value indicating whether or not to add the <see cref="CommonPrecedence" /> to the end of the chain; DEFAULT is true.</param>
         /// <param name="settingsDirectory">Optional settings root directory; DEFAULT is null and will look for a ".config" folder at execution location.</param>
-        public static void ResetConfigureSerializationAndSetValues(IReadOnlyList<string> precedenceValues, bool includeCommonPrecedenceAtEnd = true, string settingsDirectory = null)
+        /// <param name="announcer">Optional announcer to communicate setup state; DEFAULT is null.</param>
+        public static void ResetConfigureSerializationAndSetValues(IReadOnlyList<string> precedenceValues, bool includeCommonPrecedenceAtEnd = true, string settingsDirectory = null, Action<string> announcer = null)
         {
             new { precedenceValues }.Must().NotBeNull().And().NotBeEmptyEnumerable<string>().OrThrowFirstFailure();
 
-            Settings.Reset();
+            void NullAnnouncer(string message)
+            {
+                /* no-op */
+            }
 
-            ConfigureSerialization();
+            var localAnnouncer = announcer ?? NullAnnouncer;
+
+            Settings.Reset();
+            localAnnouncer(Invariant($"Called {nameof(Settings)}.{nameof(Settings.Reset)}."));
+
+            ConfigureSerialization(announcer);
 
             if (!string.IsNullOrWhiteSpace(settingsDirectory))
             {
                 Directory.Exists(settingsDirectory).Named(Invariant($"{nameof(settingsDirectory)}-{settingsDirectory}-MustExistToUse")).Must().BeTrue().OrThrowFirstFailure();
                 Settings.SettingsDirectory = settingsDirectory;
+                localAnnouncer(Invariant($"Set {nameof(Settings)}.{nameof(Settings.SettingsDirectory)} to {settingsDirectory}"));
             }
 
             var localPrecedences = precedenceValues.ToList();
@@ -82,6 +105,7 @@ namespace Naos.Recipes.Configuration.Setup
             }
 
             Settings.Precedence = localPrecedences.ToArray();
+            localAnnouncer(Invariant($"Set {nameof(Settings)}.{nameof(Settings.Precedence)} to '{localPrecedences.ToDelimitedString(",")}'"));
         }
     }
 }
