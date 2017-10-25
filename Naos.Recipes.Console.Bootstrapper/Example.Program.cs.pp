189e6aea30ca876ae9a813ccad88a059590af5cb
Lawson Caudill <lawson@caudillclan.us>
2017-10-25 10:59:26 -0300

    Added Naos.Recipes.Console.Boostrapper.
    Allows common logic to get a Console application up and running to be shared around.

---
+++ Naos.Recipes.Console.Bootstrapper/Example.Program.cs.pp
@@ -0,0 +1,56 @@
+// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ExampleProgram.cs" company="Naos">
+//    Copyright (c) Naos 2017. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace $rootnamespace$
+{
+    using System;
+
+    using CLAP;
+
+    using Its.Log.Instrumentation;
+
+    /// <summary>
+    /// Main entry point of the application.
+    /// </summary>
+    public static class ExampleProgram
+    {
+        /// <summary>
+        /// Main entry point.
+        /// </summary>
+        /// <param name="args">Arguments for application.</param>
+        /// <returns>Exit code.</returns>
+        public static int Main(string[] args)
+        {
+            try
+            {
+                /*---------------------------------------------------------------------------*
+                 * This is just a pass through to the CLAP implementation of the harness,    *
+                 * it will parse the command line arguments and provide multiple entry       *
+                 * points as configured.  The recipe will install a file named               *
+                 * 'ExampleCommandLineAbstraction' which should be renamed to                *
+                 * 'CommandLineAbstraction' to compile, this will prevent future package     *
+                 * updates from overwriting any custom logic.                                *
+                 *---------------------------------------------------------------------------*/
+                var exitCode = Parser.Run<CommandLineAbstraction>(args);
+                return exitCode;
+            }
+            catch (Exception ex)
+            {
+                /*---------------------------------------------------------------------------*
+                 * This should never be reached but is here as a last ditch effort to ensure *
+                 * errors are not lost.                                                      *
+                 *---------------------------------------------------------------------------*/
+                Console.WriteLine(string.Empty);
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
+                Console.WriteLine(string.Empty);
+                Log.Write(ex);
+
+                return 1;
+            }
+        }
+    }
+}
